cdd89ec07ce55c0477744240056a2bbd49791ca8
Ethan Ligon <ligon@ligon.org>
2017-03-14 15:06:13 -0700

    Reorganize in preparation for packaging.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CFEDemands',
+    version='0.1dev',
+    packages=['cfe',],
+    license='Creative Commons Attribution-Noncommercial-ShareAlike 4.0 International license',
+    long_description=open('README.txt').read(),
+)
