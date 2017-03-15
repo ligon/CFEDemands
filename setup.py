500e668b9c2ec362cb0f98e45f3aa33cfdb7eb33
Ethan Ligon <ligon@ligon.org>
2017-03-15 13:25:01 -0700

    Update version; forbid python 3.*.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
+import sys
+
+if not sys.version_info[0] == 2:
+    sys.exit("Sorry, Python 3 is not supported (yet)")
 
 setup(
     name='CFEDemands',
-    version='0.1',
+    version='0.1.1dev',
     author='Ethan Ligon',
     author_email='ligon@berkeley.edu',
     packages=['cfe',],
