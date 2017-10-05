19d767ea742c670844c70d012882525bd8f0149b
Ethan Ligon <ligon@ligon.org>
2017-10-04 17:49:30 -0700

    Update to version 0.1.5.0dev.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setup(
     name='CFEDemands',
-    version='0.1.4.2',
+    version='0.1.5.0dev',
     author='Ethan Ligon',
     author_email='ligon@berkeley.edu',
     packages=['cfe',],
