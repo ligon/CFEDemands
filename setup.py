aa8a9ccbbd57bbed6124b62500e4caef3dd826cb
Ethan Ligon <ligon@ligon.org>
2018-06-07 21:53:26 -0700

    Bump version to 0.1.5.2dev.

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from distutils.core import setup
 import sys
 
-#if not sys.version_info[0] == 2:
-#    sys.exit("Sorry, Python 3 is not supported (yet)")
-
 setup(
     name='CFEDemands',
-    version='0.1.5.1dev',
+    version='0.1.5.2dev',
     author='Ethan Ligon',
     author_email='ligon@berkeley.edu',
     packages=['cfe',],
