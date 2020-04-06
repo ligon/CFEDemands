4b8bdf0156a4e1ea49b34970ecf40e8e681bbf8b
Ethan Ligon <ligon@berkeley.edu>
2020-04-06 13:52:06 -0700

    Fix bug with Result.to_dataset; upload new version to pypi.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='CFEDemands',
-    version='0.2.1dev',
+    version='0.2.2dev',
     author='Ethan Ligon',
     author_email='ligon@berkeley.edu',
     packages=['cfe',],
