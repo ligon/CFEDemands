cd9a6396e4f10703dc87c31ba8389e8d0d2ac988
Ethan Ligon <ligon@berkeley.edu>
2021-10-18 13:10:54 -0700

    Fix problem import of engel_curves (moved to Demands package).

--- cfe/__init__.py
+++ cfe/__init__.py
@@ -2,7 +2,7 @@ from . import estimation
 from . import df_utils
 import demands
 from . import dgp
-from . import engel_curves
+from demands import engel_curves
 from . import input_files
 from .result import Result, from_dataset
 
