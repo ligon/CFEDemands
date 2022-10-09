bc47fab53cf4810418e4c470d4ce5d3545cf285d
Ethan Ligon <ligon@berkeley.edu>
2022-10-09 11:01:25 -0700

    Add call to drop_columns_wo_covariance to Regression.

--- cfe/__init__.py
+++ cfe/__init__.py
@@ -1,9 +1,9 @@
+import demands
 from . import estimation
 from . import df_utils
 from . import dgp
 from . import input_files
 from .result import Result, from_dataset
-import demands
 #from demands import engel_curves
 from .regression import Regression, load
-__version__='0.5.0dev',
+__version__ = '0.5.0dev'
