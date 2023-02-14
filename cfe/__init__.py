58728810518f6056488dcd9359c314d4c57c4366
Ethan Ligon <ligon@berkeley.edu>
2023-02-13 17:45:10 -0800

    Update module reference.

--- cfe/__init__.py
+++ cfe/__init__.py
@@ -4,7 +4,7 @@ from . import df_utils
 from . import dgp
 from . import input_files
 from .result import Result, from_dataset
-#from demands import engel_curves
+from consumerdemands import engel_curves
 from .regression import Regression, read_sql, read_pickle
 
 from importlib.metadata import version
